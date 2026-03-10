a8f6ad868b5e49886f7e2b637029e502a91549f3
Justice <onyekaozuru122@gmail.com>
2026-03-10 23:18:29 +0100

    TimeLock: adding variables

--- src/modules/TimeLock.sol
+++ src/modules/TimeLock.sol
@@ -5,4 +5,15 @@ import {ITimeLock} from "../interfaces/ITimeLock.sol";
 import {IProposalMg} from "../interfaces/IProposalMg.sol";
 import {AttackGuards} from "./AttackGuards.sol";
 
-contract TimeLock {}
+contract TimeLock is ITimeLock, AttackGuards {
+    mapping(bytes32 => TimeLocked) public timeLocks;
+
+    IProposalMg private _proposalMg;
+
+    address private _treasury;
+
+    uint256 public constant TIMELOCK_DELAY = 48 hours;
+
+    
+
+}

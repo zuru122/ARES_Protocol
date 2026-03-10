27823abd7dfc7d712ffe190a58470e46adc66ab9
Justice <onyekaozuru122@gmail.com>
2026-03-10 23:12:54 +0100

    Feat: TimeLock importing interfaces and Attck library

--- src/modules/TimeLock.sol
+++ src/modules/TimeLock.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.23;
 
+import {ITimeLock} from "../interfaces/ITimeLock.sol";
+import {IProposalMg} from "../interfaces/IProposalMg.sol";
+import {AttackGuards} from "./AttackGuards.sol";
+
 contract TimeLock {}

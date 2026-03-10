bc87f06b4ff6593631e0763e2a4b7c18e123f820
Justice <onyekaozuru122@gmail.com>
2026-03-10 22:23:31 +0100

    Feat: ProposalMg, importing files from created library and interface

---
+++ src/modules/ProposalMg.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import {IProposalMg} from "../interfaces/IProposalMg.sol";
+import {AttackGuards} from "../libraries/AttackGuards.sol";
+import {SignatureAuth} from "../libraries/SignatureAuth.sol";
+
+contract ProposalMg {}
